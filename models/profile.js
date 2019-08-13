940cae6ac3bd55d9faf45cffa8a1b8393c5ba8b4
lekesmile <lekesmile@hotmail.com>
2019-08-13 11:02:32 +0300

    user model update

--- models/profile.js
+++ models/profile.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema();
 
 const ProfileSchema = new Schema({
-    Name:{
-        type:String,
-        required: true
+    Name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
     },
-    Email:{
+    Email: {
         type: Email,
         required: true
     }
